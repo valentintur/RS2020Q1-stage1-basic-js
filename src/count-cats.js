f01b28fc445e414c996bb864f434e54b48003fc9
Valentin Tur <valentin.tur@pageart.pl>
2021-03-19 00:02:45 +0100

    feat: add count cat solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  //throw 'Not implemented';
+  return matrix.flat();
 };
