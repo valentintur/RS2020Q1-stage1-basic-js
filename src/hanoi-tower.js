38863237a45aab2b374dcaa3fede6ee487780b84
Walentyn <walentynpageart@MacBook-Pro-Mac.local>
2021-03-29 10:46:43 +0200

    fix: hanoi solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,8 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     //throw 'Not implemented';
-    let turns = (2 ^ disksNumber) - 1;
-    let seconds = turnsSpeed / 3600;
+    let turns = (2 ** disksNumber) - 1;
+    let turnsPerSeconds = turnsSpeed / 3600;
+    let seconds = turns / turnsPerSeconds;
     let obj = new Object();
         obj.turns = turns;
         obj.seconds = seconds;
