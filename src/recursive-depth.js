80523af5914afa966a7d1403e3210374f2dcf283
Walentyn <walentynpageart@MacBook-Pro-Mac.local>
2021-03-29 11:12:09 +0200

    feat recursice solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,12 @@
+const hasNestedArray = (arr) => arr.some(item => Array.isArray(item));
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+       // throw 'Not implemented';
+        let dep = 1;
+        if (hasNestedArray(arr)) {
+            const newArr = arr.reduce((acc, cur) => acc.concat(cur), []);
+            return depth + this.calculateDepth(newArr);
+        }
+        return depth;
     }
 };
