9504586e10f363626b83d51c75cb258ed70591f1
Walentyn <walentynpageart@MacBook-Pro-Mac.local>
2021-03-29 09:17:40 +0200

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  //throw 'Not implemented';
+  let arr = [];
+  if (typeof(members) === 'object') {
+    members.flat().forEach((elem) => {
+      if (typeof(elem) === 'string') {
+        elem = elem.trim();
+        elem = elem.slice(-elem.length, -elem.length + 1).toUpperCase();
+        arr.push(elem);
+      } else {
+        return false;
+      }  
+    });
+    console.log(`arr sort: ${arr.sort().join('')}`);
+    return arr.sort().join('');
+
+  } false;
+}
